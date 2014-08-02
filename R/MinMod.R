33cbc35b86dcb28f9beef6dcbd2cf10a694343b7
Thinkmarti <martin-stoffel@gmx.de>
2014-08-02 20:26:12 +0200

    changed MinMod slightly

--- R/MinMod.R
+++ R/MinMod.R
@@ -20,17 +20,24 @@ MinMod <- function(df, test = "F", family = gaussian) {
         ## test is "Chisq" or "F"
         
         stopdeleting <- FALSE
-        dfBase <- df
         
+        ## get df with indpendent vars
+        dfBase <- subset(df, select = 2:ncol(df))
+        
+        ## get vector with dependent var
+        depVar <- df[, 1]
+        names(depVar) <- names(dfBase)[1]
+        
+        ## which test is used
         if (test == "Chisq") {indat <- 5}
         if (test == "F") {indat <- 6}
         
         ## check case of just one independent variable and return glm if true
         if (ncol(dfBase)==2) {
-                model <- glm(dfBase[, 1] ~., data = subset(dfBase, select = 2), family = family)
+                model <- glm(depVar ~., data = subset(dfBase, select = 1), family = family)
                 print(summary(model))
-                return(list(BestModelData = df,
-                                BestModel = model))
+                return(list(ModelData = df,
+                            BestModel = model))
         }
         
         while (stopdeleting == FALSE) {
@@ -39,40 +46,40 @@ MinMod <- function(df, test = "F", family = gaussian) {
                 numVars <- ncol(dfBase)
                 
                 # model1
-                model1 <- glm(dfBase[, 1] ~., data = subset(dfBase, select = 2:numVars), family = family)
+                model1 <- glm(depVar ~., data = dfBase, family = family)
                 
                 # create pvalue vector
                 pvalues <- vector()
                 
                 ## delete every variable, make comparison and take the least significant out
-                for (i in 2:ncol(dfBase)) {
+                for (i in 1:ncol(dfBase)) {
                         
                         # Remove var
                         del <- i
-                        dftemp <- dfBase[, -del]
+                        dfTemp <- dfBase[, -del]
                         
                         # compute glm
-                        model2 <- glm(dfBase[, 1] ~., data = subset(dftemp, select = 2:(numVars-1)), family = family)
+                        model2 <- glm(depVar ~., data = dfTemp, family = family)
                         
                         # ANOVA for model comparison
                         modelCompare <- anova(model1, model2, test = test)
                         
                         # get p values in vector
-                        pvalues[i-1] <- modelCompare[2, indat]
+                        pvalues[i] <- modelCompare[2, indat]
                         
                 }
                 
-                remove <- which.max(pvalues) + 1 ## +1 because dependent variable is in column one of df
+                remove <- which.max(pvalues)  ## +1 because dependent variable is in column one of df
                 
                 # stop if deleting variable with highest p value leads to a significant different model
                 
         
-                if (pvalues[remove-1] <= 0.05){
+                if (pvalues[remove] <= 0.05){
                         stopdeleting  <- TRUE
                         bestmodeldf <- dfBase
                         
-                # stop if just one variable left
-                } else if ((pvalues[remove-1] > 0.05) & ((numVars-1)==2)){
+                # stop if just one variable left in dfTemp
+                } else if ((pvalues[remove] > 0.05) & ((numVars)==2)){
                         stopdeleting <- TRUE
                         bestmodeldf <- subset(dfBase, select = -remove)
                         
@@ -85,9 +92,11 @@ MinMod <- function(df, test = "F", family = gaussian) {
                 
         }
         
-        bestmodel <- glm(bestmodeldf[, 1] ~., data = subset(bestmodeldf, select = 2:ncol(bestmodeldf)), family = family)
-        
+        bestmodel <- glm(depVar ~., data = bestmodeldf, family = family)
         print(summary(bestmodel))
+        
+        bestmodeldf <- as.data.frame(cbind(depVar,bestmodeldf))
+        
         output <- list(ModelData = bestmodeldf,
                        BestModel = bestmodel)
         output
