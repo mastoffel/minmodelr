6dcf831390f3f1e1a1ef083b3c3e423deec5b5be
Thinkmarti <martin.adam.stoffel@gmail.com>
2015-05-11 15:57:08 +0200

    package doc

---
+++ R/minmodelr.R
@@ -0,0 +1,12 @@
+#' minmodelr: A (personal) package for automated deletion testing and single variable deletion testing
+#' 
+#' The minmodelr package provides a functions for deletion testing according to Crawley
+#' and on function for single variable deletion testing.
+#' 
+#' @section minmodelr functions:
+#' \code{\link{MinMod}}
+#' \code{\link{DelTestVar}}
+#' 
+#' @docType package
+#' @name minmodelr
+NULL
